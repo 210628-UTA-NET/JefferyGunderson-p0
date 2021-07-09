25a854b4f3389d4470048647a10a10ca3efc4615
Jeff Gunderson <jgunderson819@gmail.com>
2021-07-09 08:05:39 -0500

    Added StorefrontMenu logic

---
+++ StoreApp/SAUI/StoreFrontMenu.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace SAUI
+{
+    public class StoreFrontMenu : IMenu
+    {
+        public string _location { get; set; }
+        public static StoreFrontMenu newMenu = new StoreFrontMenu();
+        public void storeLocation(StoreFront p_storeFront)
+        {
+            newMenu._location = p_storeFront.Name;
+        }
+        public void Menu()
+        {
+            Console.WriteLine("Welcome to " + newMenu._location);
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("[3] View Order History");
+            Console.WriteLine("[2] Manage Inventory");
+            Console.WriteLine("[1] Find a Different StoreFront");
+            Console.WriteLine("[0] Go Back");
+        }
+
+        public MenuType UserInput()
+        {
+            string userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "0":
+                    return MenuType.MainMenu;
+                case "1":
+                    newMenu._location = "";
+                    return MenuType.FindStoreFrontMenu;
+                case "2":
+                    return MenuType.StoreFrontInventoryMenu;
+                case "3":
+                //  return MenuType.StoreFrontOrderHistory
+                case "4":
+                    return MenuType.OrderMenu;
+                default:
+                    return MenuType.StoreFrontMenu;
+            }
+        }
+    }
+}

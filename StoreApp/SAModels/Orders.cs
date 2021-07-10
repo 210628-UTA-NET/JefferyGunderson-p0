1322be8f9d4b96239503005285cf4fa307d0ad26
Jeff Gunderson <jgunderson819@gmail.com>
2021-07-10 15:38:05 -0500

    Changes to orders logic

--- StoreApp/SAModels/Orders.cs
+++ StoreApp/SAModels/Orders.cs
@@ -4,8 +4,23 @@ namespace SAModels
 {
     public class Orders
     {
+
+        protected int Order_Id
+        {
+            get
+            {
+                return Order_Id;
+            }
+        }
+        protected string[] LineItems { get; set; }
         protected string[] OrderLineItems { get; set; }
         protected string Location { get; set; }
-        protected double Price { get; set; }
+        protected decimal Price
+        {
+            get
+            {
+                return Price;
+            }
+        }
     }
 }

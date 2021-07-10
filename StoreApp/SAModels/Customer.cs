46105cd027868b58ff24dd9acc12586fab7a91c8
Jeff Gunderson <jgunderson819@gmail.com>
2021-07-10 15:37:12 -0500

    changes to customer logic

--- StoreApp/SAModels/Customer.cs
+++ StoreApp/SAModels/Customer.cs
@@ -1,23 +1,133 @@
 using System;
+using System.Text.RegularExpressions;
+
 
 namespace SAModels
 {
     public class Customer
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
-        public string Email { get; set; }
-        public object[] ListofOrders { get; set; }
+
+
+
+        public int Customer_Id
+        {
+            get
+            {
+                return Customer_Id;
+            }
+
+        }
+        public string Email
+        {
+            get
+            {
+                return Email;
+            }
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return FirstName;
+            }
+            set
+            {
+                if (!Regex.IsMatch(value, @"^[A-Za-z .]+$"))
+                {
+                    throw new Exception("FirstName only holds letters.");
+                }
+                FirstName = value;
+            }
+        }
+        public string AddressLine1
+        {
+            get
+            {
+                return AddressLine1;
+
+            }
+
+
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return LastName;
+            }
+
+            set
+            {
+                if (!Regex.IsMatch(value, @"^[A-Za-z .]+$"))
+                {
+                    throw new Exception("FirstName only holds letters.");
+                }
+                LastName = value;
+            }
+
+        }
+        public string AddressLine2
+        {
+            get
+            {
+                return AddressLine1;
+
+            }
+
+
+        }
+        public string City
+        {
+            get
+            {
+                return City;
+            }
+
+            set
+            {
+                if (!Regex.IsMatch(value, @"^[A-Za-z .]+$"))
+                {
+                    throw new Exception("FirstName only holds letters.");
+                }
+                City = value;
+            }
+
+        }
+        public string State
+        {
+            get
+            {
+                return State;
+            }
+
+            set
+            {
+                if (!Regex.IsMatch(value, @"^[A-Za-z .]+$"))
+                {
+                    throw new Exception("FirstName only holds letters.");
+                }
+                State = value;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         //overrides default string function allowing you to output object parameters:: otherwise you receive "namespace.class"
 
-        public override string ToString()
-        {
-            return $"Name: {Name}\nAddress: {Address}\nEmail: {Email}";
-        }
+        public override string ToString() => $" Customer_Id: {Customer_Id}\n FirstName: {FirstName}\n LastName: {LastName}\n AddressLine1: {AddressLine1}\n AddressLine2: {AddressLine2}\n City: {City} \n State: {State}\n Email: {Email}";
 
     }
-
 }

1da935f4397190f4003c5a18ab9baa277904da96
Jeff Gunderson <jgunderson819@gmail.com>
2021-07-12 16:02:44 -0500

    change LineItems.cs

--- StoreApp/SAModels/LineItems.cs
+++ StoreApp/SAModels/LineItems.cs
@@ -5,17 +5,17 @@ namespace SAModels
     public class LineItems
     {
         public int _LineItem_Id;
-        public int LineItem_Id 
-        
+
+
 
 
         public LineItems(string product, int quantity, int lineItem_Id)
         {
             this.Product = product;
             this.Quantity = quantity;
-            this.LineItem_Id = lineItem_Id
+
         }
-      
+
         private string Product { get; set; }
         private int Quantity { get; set; }
     }

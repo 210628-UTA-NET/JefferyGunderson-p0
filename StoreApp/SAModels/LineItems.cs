f9068c13c66ca602c5aec6c15ab8ad957b7f4d23
Jeff Gunderson <jgunderson819@gmail.com>
2021-07-12 10:26:56 -0500

    Added LineItems.cs to Entities

--- StoreApp/SAModels/LineItems.cs
+++ StoreApp/SAModels/LineItems.cs
@@ -4,7 +4,19 @@ namespace SAModels
 {
     public class LineItems
     {
-        private string Product_Id { get; set; }
+        public int _LineItem_Id;
+        public int LineItem_Id 
+        
+
+
+        public LineItems(string product, int quantity, int lineItem_Id)
+        {
+            this.Product = product;
+            this.Quantity = quantity;
+            this.LineItem_Id = lineItem_Id
+        }
+      
+        private string Product { get; set; }
         private int Quantity { get; set; }
     }
 }
